979806419faf4621d46d3d923fb79f9851feddef
victorjaquez <victorajaquez@gmail.com>
2018-09-16 16:17:28 -0700

    git set-up

---
+++ src/actions/index.js
@@ -0,0 +1,15 @@
+import axios from "axios";
+
+const API_KEY = "51bbae292d3c0f03ef5630cbac736c44";
+const ROOT_URL = `https://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = "FETCH_WEATHER";
+
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},us`;
+  const request = axios.get(url);
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  };
+}
